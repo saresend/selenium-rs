4903508cb3450fba66f647cd25f8e8e0e2a05cb1
saresend <saresend@usc.edu>
2018-09-11 18:02:32 -0700

    adding some documentation to lib.rs and webdriver

--- src/lib.rs
+++ src/lib.rs
@@ -1,3 +1,45 @@
+/*! 
+    This library provides a client API for the selenium webdriver specification: 
+    [https://www.w3.org/TR/webdriver1/]
+
+    #Usage 
+
+    ```toml
+    [dependencies]
+    selenium_rs = "0.1"
+    ```
+    
+    and add this to your root crate: 
+    ```rust
+    extern crate selenium_rs;
+    ```
+
+
+    ## Requirements 
+
+    Selenium-rs requires that there is an instance of the selenium-webdriver server running
+    to make requests against. 
+
+    As of right now the easiest way to get this is to download the standalone from:
+    [https://www.seleniumhq.org/download/], and then running it (requires Java 1.8+) 
+
+
+    # Example: Make a google search, programmatically 
+
+    ```rust 
+    use selenium_rs::webdriver::{Browser, WebDriver, Selector};
+
+    let mut driver = WebDriver::new(Browser::Chrome);
+    driver.start_session();
+    driver.navigate("http://google.com");
+    let search_bar = driver.query_element(Selector::CSS, ".gLFyf").unwrap();
+    search_bar.type_text("selenium-rs github");
+    let search_button = driver.query_element(Selector::CSS, "#gbqfbb").unwrap();
+
+    search_button.click();
+    ```
+*/
+
 extern crate reqwest;
 extern crate url;
 
--- src/webdriver.rs
+++ src/webdriver.rs
@@ -20,6 +20,9 @@ pub enum Selector {
     XPath,
 }
 
+/// The WebDriver is the primary way by which interaction
+/// is managed between the Selenium-Webdriver Server, and
+/// our client.
 pub struct WebDriver {
     browser: String,
     client: reqwest::Client,
@@ -28,6 +31,9 @@ pub struct WebDriver {
 
 // Contains Creation Methods
 impl WebDriver {
+    /// Constructs a new Webdriver with the specific browser.
+    /// TODO: Make sure and add testing to verify that it supports
+    /// firefox properly
     pub fn new(browser: Browser) -> WebDriver {
         let browser = get_browser_string(browser);
         WebDriver {
@@ -40,6 +46,9 @@ impl WebDriver {
 
 // Contains Session Handling
 impl WebDriver {
+    /// Actually starts and creates a session on the server,
+    /// collecting the session ID on success, and returning an error
+    /// on failure
     pub fn start_session(&mut self) -> reqwest::Result<()> {
         let body = NewSessionRequest::new(&self.browser);
         let url = construct_url(vec!["session/"]);
@@ -54,7 +63,8 @@ impl WebDriver {
         self.session_id = Some(response.get_session_id());
         Ok(())
     }
-
+    /// Returns the current url of the browsing context. See examples for
+    /// more details on how this is used
     pub fn get_current_url(&self) -> reqwest::Result<String> {
         let url = construct_url(vec![
             "session/",
@@ -66,14 +76,20 @@ impl WebDriver {
         Ok(response.get_title())
     }
 
+    /// Deletes the session, consuming itself in the process
+    /// TODO: Since we must manually add a new session, it would definitely stand to reason that we
+    /// should not consume the webdriver here, or, be tied specifically to a single session
     pub fn delete_session(self) {
-        let url = construct_url(vec!["session/", &self.session_id.clone().unwrap()]);
+        let url = construct_url(vec!["session/", &self.session_id.unwrap()]);
         let _ = self.client.delete(url).send();
     }
 }
 
 // Contains Navigation Handling
 impl WebDriver {
+    /// Navigates the current browsing session to the reqwested url. This will
+    /// also wait until the browser has finished executing the request, meaning that
+    /// future calls may assume we've reached the appropriate url
     pub fn navigate(&self, url: &str) -> reqwest::Result<()> {
         let sess_id = self.session_id.clone().unwrap();
         let nav_url = construct_url(vec!["session/", &(sess_id + "/"), "url"]);
@@ -90,6 +106,7 @@ impl WebDriver {
 
 // Contains Element Handling
 impl WebDriver {
+    /// Requests an elements from the webpage, given the specified selector and query string
     pub fn query_element(&self, selector: Selector, query: &str) -> reqwest::Result<Element> {
         let sess_id = self.session_id.clone().unwrap();
         let url = construct_url(vec!["session/", &(sess_id + "/"), "element"]);
